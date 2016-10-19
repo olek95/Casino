e926ddaf0fe0279cd53f10d3bb73fbbd9fe06946
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-10-19 14:15:40 +0200

    Update Blackjack.java
    dodanie komentarzy, drobne modyfikacje metod

--- Blackjack.java
+++ Blackjack.java
@@ -5,6 +5,12 @@ import java.util.HashMap;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
+/**
+ * Obiekt klasy <code>Blackjack</code> będzie reprezentował grę Blackjack. 
+ * Gracz wygrywa w tej grze jeśli ma więcej lub tyle samo punktów co krupiej oraz
+ * mniej lub równo 21 punktów.
+ * @author AleksanderSklorz
+ */
 public class Blackjack {
      private static Blackjack game;
     private static int[] playerCards, croupierCards;
@@ -16,27 +22,35 @@ public class Blackjack {
             game = new Blackjack();
         return game;
     }
-    public boolean play(){
-        playerCards = new int[9];
+    /**
+     * Uruchamia grę. Tasuje karty (jest 52 kart) oraz rozdaje po dwie początkowe 
+     * karty graczowi i krupierowi.
+     */
+    public void play(){
+        playerCards = new int[9]; // maksymalnie gracz może mieć 9 karty w talii
         croupierCards = new int[9];
         availableCards = new HashMap();
         for(int i = 2; i <= 14; i++)
-            availableCards.put(i, 4);
-        Arrays.fill(playerCards, 0);
+            availableCards.put(i, 4); // każda wartość występuje po 4 razy
+        Arrays.fill(playerCards, 0); // 0 oznacza iż w tym miejscu brak karty
         Arrays.fill(croupierCards, 0);
         for(int i = 0; i < 2; i++){
             hit(playerCards);
             hit(croupierCards);
         }
-        return false;
     }
-    public void hit(int[] ownerCards){
+    /**
+     * Pobiera jedną kartę.
+     * @param ownerCards tablica właściciela do której ma trafić wyciągnięta karta
+     * @return czy udało się pobrać kartę
+     */
+    public boolean hit(int[] ownerCards){
         Random rand = new Random();
         int i = findZero(ownerCards);
         int drawn;
         if(i != ownerCards.length){
             do{
-                drawn = rand.nextInt(13) + 2;
+                drawn = rand.nextInt(13) + 2; // dodaję 2 bo karty numeruje się od 2
                 if(drawn == 14)
                     if(ownerCards == playerCards) drawn = aceOneOrEleven();
                     else if(getPoints(croupierCards) + 11 > 21) drawn = 15;
@@ -44,13 +58,20 @@ public class Blackjack {
             ownerCards[i] = drawn;
             if(drawn == 15) drawn--;
             availableCards.replace(drawn, availableCards.get(drawn) - 1);
+            return true;
         }
+        return false;
     }
+    /**
+     * Zamienia duplikat karty na inną wyciągniętą z talii.
+     * @param i numer karty (liczony od 1) którą gracz chce wyciągnąć
+     * @return czy udało się zamienić
+     */
     public boolean split(int i){
         int card = playerCards[i - 1];
         int index = 0;
         do{
-            if(index != i - 1 && playerCards[index] == card){
+            if(index != i - 1 && playerCards[index] == card){ // sprawdza czy rzeczywiście istnieje duplikat
                 playerCards[i - 1] = 0;
                 hit(playerCards);
                 availableCards.replace(card, availableCards.get(card) + 1);
@@ -58,14 +79,23 @@ public class Blackjack {
             }
             index++;
         }while(index < playerCards.length);
-        return false;
+        return false; // jeśli przeszło po wszystkich kartach i nie znaleziono duplikatu - zwraca false
     }
-    public int aceOneOrEleven(){
-        int value = JOptionPane.showOptionDialog(null, "Pobrałeś asa. Czy ten as ma mieć wartośc 1 czy 11?", null, 
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"1", "11"}, null);
-        if(value == 0) return 14;
-        else return 15;
+    private int aceOneOrEleven(){
+        int value;
+        do{
+            value = JOptionPane.showOptionDialog(null, "Pobrałeś asa. Czy ten as ma mieć wartośc 1 czy 11?", null, 
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, new String[]{"1", "11"}, null);
+        }while(value == JOptionPane.CLOSED_OPTION);
+        return value == 0 ? 14 : 15; // 1 będzie symbolizowana przez 14, a 11 przez 15.
     }
+    /**
+     * Zwraca sumę wszystkich punktów uzyskanych przez właściciela kart.
+     * Karty od 2 do 10 mają wartości odpowiednio od 2 do 10; walet, dama i krój
+     * mają wartość 10, natomiast As ma wartość 1 lub 11.
+     * @param ownerCards tablica kart których wartości maja zostać zsumowane.
+     * @return suma wartości kart.
+     */
     public int getPoints(int[] ownerCards){
         int i = 0, sum = 0;
         boolean end = false;
@@ -79,23 +109,33 @@ public class Blackjack {
         }
         return sum; 
     }
+    /**
+     * Sprawdza czy gracz wygrał w grę. Wygra wtedy, gdy suma punktów gracza
+     * jest większa lub równa sumie punktów krupiera oraz mniejsza lub równa 21.
+     * @return czy gracz wygrał w grę.
+     */
     public boolean isWin(){
         int playerSum = getPoints(playerCards);
         return playerSum >= getPoints(croupierCards) && playerSum <= 21;
     }
+    /**
+     * Zwraca wszystkie karty gracza (wraz z miejscami równymi 0).
+     * @return tablica z kartami gracza.
+     */
     public int[] getPlayerCards(){
         return playerCards;
     }
+    /**
+     * Zwraca wszystkie karty krupiera (wraz z miejscami równymi 0).
+     * @return tablica z kartami krupiera.
+     */
     public int[] getCroupierCards(){
         return croupierCards;
     }
     private int findZero(int[] ownerCards){
         for(int i = 0; i < ownerCards.length; i++)
             if(ownerCards[i] == 0) return i;
-        return ownerCards.length;
-    }
-    public HashMap<Integer, Integer> getA(){
-        return availableCards;
+        return ownerCards.length; // jeśli nie znajdzie 0, zwraca długość
     }
 }
 
